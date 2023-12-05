50d4f4e9346fa61ffccbc6debce1e6b63b8415f2
Mahikshit Kurapati <mahikshitk@gwu.edu>
2023-12-04 23:37:22 -0500

    updated the model in sample

--- code/sample.py
+++ code/sample.py
@@ -1,11 +1,12 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import tensorflow as tf
-from tensorflow.keras.layers import Conv2D,Dense,MaxPooling2D,Flatten,BatchNormalization,Dropout
+from tensorflow.keras.layers import Conv2D,Dense,MaxPooling2D,Flatten,BatchNormalization,Dropout,Activation
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.applications import VGG16
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications.resnet import preprocess_input
+from tensorflow.keras.regularizers import l2
 import matplotlib.pyplot as plt
 #import cv2 as cv
 from sklearn.utils import shuffle
@@ -15,9 +16,9 @@ import os
 
 cwd = os.getcwd()
 curr_path = os.path.dirname(cwd)
-classification_path = os.path.join(curr_path, 'Classification')
+classification_path = os.path.join(curr_path, 'the_wildfire_dataset' + '/' + 'the_wildfire_dataset')
 train_dir = os.path.join(classification_path, 'train')
-valid_dir = os.path.join(classification_path, 'valid')
+valid_dir = os.path.join(classification_path, 'val')
 test_dir = os.path.join(classification_path, 'test')
 
 test_classes = os.listdir(test_dir)
@@ -79,17 +80,37 @@ for layer in VGG16_model.layers :
 
 from keras import activations
 model = Sequential()
+
+# VGG16 base model
 model.add(VGG16_model)
+
+# Additional Conv2D layers before Flatten
+model.add(Conv2D(64, (3, 3), padding='same'))
 model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+
+# Existing architecture
 model.add(MaxPooling2D((2,2)))
 model.add(Flatten())
-model.add(Dense(1024,activation = 'relu'))
-model.add(Dropout(0.3))
-model.add(Dense(512,activation = 'relu'))
-model.add(Dropout(0.3))
-model.add(Dense(256,activation = 'relu'))
-model.add(Dense(128,activation = 'relu'))
-model.add(Dense(num_classes,activation = 'softmax'))
+model.add(BatchNormalization())
+
+# More dense layers with increased neurons and regularization
+model.add(Dense(2048, activation='relu', kernel_regularizer=l2(0.001)))
+model.add(Dropout(0.4))
+model.add(Dense(1024, activation='relu', kernel_regularizer=l2(0.001)))
+model.add(Dropout(0.4))
+model.add(Dense(512, activation='relu', kernel_regularizer=l2(0.001)))
+model.add(Dropout(0.4))
+model.add(Dense(256, activation='relu'))
+model.add(Dense(128, activation='relu'))
+
+# Output layer
+model.add(Dense(num_classes, activation='softmax'))
+
+# Model summary
 model.summary()
 
 model.compile(
@@ -114,7 +135,7 @@ report = classification_report(true_label,predictions)
 print(report)
 
 conf_mat = confusion_matrix(true_label,predictions)
-sns.heatmap(conf_mat ,fmt='g',annot = True , cmap='Blues' , xticklabels=test_classes , yticklabels = test_classes,)
+sns.heatmap(conf_mat ,fmt='g',annot = True , cmap='Blues' , xticklabels=test_classes , yticklabels = test_classes)
 plt.xlabel('Predictions')
 plt.ylabel('True label')
 plt.title('Confusion Matrix')
